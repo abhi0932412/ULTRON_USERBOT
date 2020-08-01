586651fafa47ec584fca910e824124dc6545b4d0
abhi0932412 <68628449+abhi0932412@users.noreply.github.com>
2020-08-01 08:29:58 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://media.giphy.com/media/S6fBfSD4ksL5xUWA0i/giphy.gif"
+PM_IMG = "https://64.media.tumblr.com/72d81970104d4d87839c548db62a7abe/tumblr_ndvyfxTlsJ1u0cfhio6_500.gifv"
 pm_caption = "`ULTRON IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
@@ -15,7 +15,7 @@ pm_caption += "**Current Branch** : `master`\n"
 pm_caption += "**ULTRON OS** : `3.14`\n"
 pm_caption += "**Current Sat** : `BannerGangSat-2.25`\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**ULTRON Database** : `JARVIS- Working Properly`\n\n"
+pm_caption += "**ULTRON Database** : `ULTRON- Working Properly`\n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
