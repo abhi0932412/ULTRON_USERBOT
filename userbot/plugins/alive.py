2e91042e81cca9a07ffdaddfcda6efd0b3c11ad5
abhi0932412 <68628449+abhi0932412@users.noreply.github.com>
2020-08-01 08:20:29 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,16 +6,16 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://i.pinimg.com/474x/4d/af/0a/4daf0aaec6098c561e327c70cf3578c2--superheroes-hulk-party.jpg"
-pm_caption = "`TECH IS:` **ONLINE**\n\n"
+PM_IMG = "https://media.giphy.com/media/S6fBfSD4ksL5xUWA0i/giphy.gif"
+pm_caption = "`ULTRON IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
 pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**TECH OS** : `3.14`\n"
+pm_caption += "**ULTRON OS** : `3.14`\n"
 pm_caption += "**Current Sat** : `BannerGangSat-2.25`\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "**ULTRON Database** : `JARVIS- Working Properly`\n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
